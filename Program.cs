4295ca2bba56380c70c3df254a14bcc94a34513e
Maram-Bdeer <79007122+Maram-Bdeer@users.noreply.github.com>
2022-04-02 22:28:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ Console.WriteLine("Choose an option:");
+    Console.WriteLine("1) add user");
+    Console.WriteLine("2) Remove user");
+    Console.WriteLine("3) Edit user");
+     Console.WriteLine("4) Exit");
+    Console.Write("\r\nSelect an option: ");
+    int id , age;
+string name;
+User u;
+    switch (Console.ReadLine())
+    {
+        case "1":
+           Add();
+            return true;
+        case "2":
+            Remove();
+            return true;
+         case "3":
+           edit(id,age,name,u);
+            return true;
+        
+        case "4":
+            return false;
+        default:
+            return true;
+    }
+}
